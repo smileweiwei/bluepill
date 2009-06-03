81077492c6ad654041d117e5c7c358d72d244b5f
p3net.tech <p3net.tech@d9233032-4f49-11de-a6a3-df26d2c71dbe>
2009-06-03 03:22:17 +0000

    A first look at a modules system.

--- trunk/common.php
+++ trunk/common.php
@@ -18,9 +18,32 @@ function __autoload()
 	/**
 	* The Savant template engine and our own implementation
 	*/
-	require 'includes/third_party/Savant3-3.0.0/Savant3.php';
-	require 'includes/class_template.php';
+	require BASE_DIR . 'includes/third_party/Savant3-3.0.0/Savant3.php';
+	require BASE_DIR . 'includes/class_template.php';
+	
+	/**
+	* adodb5 (for database abstraction)
+	*/
+	require BASE_DIR . 'includes/third_party/adodb5/adodb.inc.php';
+	
+	/**
+	* Our modules system
+	*/
+	require BASE_DIR . 'includes/abstract_module.php';
+	require BASE_DIR . 'includes/class_module_master.php';
 }
 
+define('BASE_DIR', getcwd() . '/');
+
 $template = new template();
+
+require 'config.php';
+$db = NewADOConnection($db_info['dbms']);
+$db->Connect($db_info['host'], $db_info['user'], $db_info['pass'], $db_info['name']);
+define('DB_PREFIX', $db_info['pfix']);
+unset($db_info);
+
+require BASE_DIR . 'includes/constants.php';
+
+$p_master = new module_master();
 ?>
---
+++ trunk/includes/class_module_master.php
@@ -0,0 +1,97 @@
+<?php
+/**
+*
+* @package bluepill
+* @version $Id$
+* @copyright (c) 2009 p3net
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+*	Controls the loading and rendering of modules
+*/
+class module_master
+{
+	/**
+	* Modules that are currently loaded
+	* 'name' refers to the modules name given to the user
+	* 'mode' refers to the module mode passed to the leader
+	* 'internal' refers to the internal file name
+	* 'instant' refers to the function name within the script that the class is instantiated as
+	* Each module looks like array('name' => '', 'class' => '', 'mode' => '', 'internal => '', 'instant' => '')
+	*/
+	private $_modules = array();
+	
+	/**
+	* The module output to the template
+	*/
+	private $_module_output = array();
+	
+	/**
+	* Load a module
+	* @param string $internal_name The name used within the filesystem
+	* @param string $mode The module mode
+	* @param array $args The arguments passed to the module
+	* @param string $module_class The parent class (folder) of the module
+	* @return bool Returns true if instantiated, false otherwise
+	*/
+	function load($internal_name, $mode = '', $args = array(), $module_class = '')
+	{
+		// First we need to make sure the module exists
+		if (!file_exists(BASE_DIR . 'modules/'  (empty($module_class) ? '' : $module_class . '/') . $internal_name . '.php') || class_exists($internal_name)))
+		{
+			return false;
+		}
+		
+		require BASE_DIR . 'modules/' . (empty($module_class) ? '' : $module_class . '/') . $internal_name . '.php';
+		
+		// Since some modules specify dependencies, see if those are loaded and return false if they are not
+		if (sizeof($internal_name::_dependencies))
+		{
+			foreach($internal_name::_dependencies as $depends)
+			{
+				if (!in_array($this->_modules, $depends))
+				{
+					return false;
+				}
+			}
+		}
+		
+		$gen_name = '_' . md5($internal_name . time());
+		
+		$this->$gen_name = new $internal_name;
+		
+		$module_details = $internal_name::_name = array(
+			'name'		=> $internal_name::_name,
+			'class'		=> $internal_name::_class,
+			'mode'		=> $mode,
+			'internal'	=> $internal_name::_internal,
+			'instant'	=> $gen_name,
+		);
+		
+		array_push($this->_modules, $module_details);
+		
+		$this->$gen_name->module_setup($mode, $args);
+	}
+	
+	/**
+	* Send module output (all of those loaded) to template
+	* @param string $class If class is specified, only the output of modules of said class will be loaded
+	*/
+	function load_to_template($class = '')
+	{
+		global $template;
+		
+		foreach($this->_modules as $load_module)
+		{
+			if (empty($class) || $load_module['class'] == $class)
+			{
+				array_push($this->_module_output, array($load_module['name'] => $this->$load_module['instant']->get_output));
+			}
+		}
+		
+		$template->assign('modules', $this->module_output, true, false);
+	}
+}
+?>
